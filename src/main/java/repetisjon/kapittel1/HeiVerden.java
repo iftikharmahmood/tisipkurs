e9efcde62eac0ee90cd2ce3f1c6401bc09832741
Iftikhar Mahmood <47450543+iftikharmahmood@users.noreply.github.com>
2020-09-25 15:26:36 +0200

    nytt prosjekt

---
+++ src/main/java/repetisjon/kapittel1/HeiVerden.java
@@ -0,0 +1,12 @@
+package repetisjon.kapittel1;
+
+/**
+ * HeiVerden.java
+ * kopi av HelloWorld.java
+ */
+
+public class HeiVerden {
+    public static void main(String[] args){
+        System.out.println("Hei verden");
+    }
+}
